2af459eea619e0e07119d5d4391bd1727f9ae206
Nick Campbell <nc@zaikio.com>
2021-04-21 17:16:57 +0100

    Set http.use_ssl when working with HTTPS connections

--- test/zaikio/directory_cache_test.rb
+++ test/zaikio/directory_cache_test.rb
@@ -1,12 +1,19 @@
 require "test_helper"
 
 class Zaikio::JWTAuth::DirectoryCacheTest < ActiveSupport::TestCase
+  def setup
+    Zaikio::JWTAuth.configure do |config|
+      config.environment = :test
+      config.app_name = "test_app"
+    end
+  end
+
   test "when server responds with a 429 response" do
     stub_request(:get, "http://hub.zaikio.test/foo.json")
       .to_return(status: 429, body: "Retry later", headers: { "Content-Type" => "text/plain" })
 
     assert_raises(Zaikio::JWTAuth::DirectoryCache::BadResponseError) do
-      Zaikio::JWTAuth::DirectoryCache.fetch("foo.json")
+      assert Zaikio::JWTAuth::DirectoryCache.fetch("foo.json", invalidate: true)
     end
   end
 end
